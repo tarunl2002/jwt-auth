f197e7864cfa3272ec3fe587448ef5cf880e7445
Tarun Lalwani <tarunlalwani2002@gmail.com>
2023-10-02 16:55:55 +0530

    database connected

--- config/database.js
+++ config/database.js
@@ -1,18 +1,19 @@
 const mongoose = require("mongoose");
 
-const MONGO_URI = process.env.MONGO_URI;
+const { MONGO_URI } = process.env;
 
 exports.connect = () => {
-    mongoose.connect(MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
+  mongoose
+    .connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     })
     .then(() => {
-        console.log("successfully connected to database");
+      console.log("Successfully connected to database");
     })
     .catch((error) => {
-        console.log("database connection failed");
+      console.log("database connection failed. exiting now...");
+      console.error(error);
+      process.exit(1);
     });
 };
